6b07f6feb8d3b9632678f2251de7669d31595078
Adrian Letchford <me@dradrian.com>
2016-05-08 11:02:34 +0100

    added zooming

--- infographic.php
+++ infographic.php
@@ -2,13 +2,22 @@
 
 include "header.php";
 
-
 $image = $_GET["image"];
-
 ?>
 
+<script src='js/jquery.zoom.js'></script>
+<script>
+	$(document).ready(function(){
+		$('#ex2').zoom({ on:'grab' });
+	});
+</script>
+
 <div class="container">
-	<img class="infographic" src="images/<?php echo $image; ?>.png"/>
+
+	<span class='zoom' id='ex2'>
+		<img class="infographic" src="images/<?php echo $image; ?>.png"/>
+	</span>
+
 </div>
 
 
